7a2466b4f57e0092dfb8094bd6341b4d2a4f08d5
Ruito Homma <166892044+rhomma401@users.noreply.github.com>
2024-06-07 13:03:41 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sample-app-zdyo.onrender.com'
+  host = 'sample-app-zdyo.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
